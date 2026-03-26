421ab1d311b6995c2807165fbff1f8f96f12639f
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-03-26 15:27:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Listing = require('./models/listing.js')
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 const path = require('path');
 const methodOverride = require("method-override");
-const ejsMate = require('ejs-mate');
+const ejsMate = require('ejs-mate'); 
 
 
 
