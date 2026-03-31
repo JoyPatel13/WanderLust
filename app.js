030233961e3d4d70a881c1edc079cb5479026d8a
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-03-31 13:15:16 +0530

    add signup authetication

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ const path = require('path');
 const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate'); 
 const ExpressError = require('./utils/ExpressError.js');
-const listings = require('./routes/listing.js');
-const reviews = require('./routes/review.js');
+
+const listingsRouter = require('./routes/listing.js');
+const reviewsRouter = require('./routes/review.js');
+const userRouter = require("./routes/user.js");
+
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require("./models/user.js");
+const user = require('./models/user.js');
+
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -45,6 +53,14 @@ async function main() {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 
 app.get("/" ,(req,res)=>{
     res.redirect("/listings");
@@ -57,12 +73,14 @@ app.use((req,res,next)=>{
     res.locals.error = req.flash("error")
     next();
 
-})
+});
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews" , reviews)
 
+
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews" , reviewsRouter)
+app.use("/" , userRouter);
 
 
 app.use((req, res, next) => {
