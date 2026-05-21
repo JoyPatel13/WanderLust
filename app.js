1133f61e60125d664fe5b9e507303dcc743ebd5c
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-05-21 16:13:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.engine('ejs' , ejsMate);
 app.use(express.static(path.join(__dirname,"/public")))
 
 const store =MongoStore.create({
-    mongoUrl: MONGO_URL,
+    mongoUrl: dbUrl,
     crypto:{
         secret :process.env.SECRET,
     },
@@ -67,7 +67,7 @@ main().then(() => {
 })
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 app.use(session(sessionOptions));
 app.use(flash());
